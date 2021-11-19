99c2b5cac79fc76220dd96aa85d3d44f2700ecf6
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 20:57:41 +0530

    add build file

---
+++ jenkinsfile_build.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agent any
+    stages{
+        stage("checkout code"){
+            steps{
+                println "clone our code to our repository"
+                sh "ls -l"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
+                sh "ls -lart ./*"
+
+            }
+        }
+        stage("build code"){
+            steps{
+                println "mvn clean package"
+                sh "mvn clean package"
+                sh "ls -l target/"
+            }
+        }
+        stage("upload artifacts to s3"){
+            steps{
+                println "uploading artifacts to s3 bucket"
+            }
+        }
+    }
+}
