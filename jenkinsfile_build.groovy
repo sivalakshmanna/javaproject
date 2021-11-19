1a61146cf1144f220ee8983e1024d8934a7c6156
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 21:00:41 +0530

    add build file

--- jenkinsfile_build.groovy
+++ jenkinsfile_build.groovy
@@ -20,6 +20,8 @@ pipeline{
         stage("upload artifacts to s3"){
             steps{
                 println "uploading artifacts to s3 bucket"
+                sh "echo $BUILD_NUMBER"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://sivabandela/master/${BUILD_NUMBER}/"
             }
         }
     }
