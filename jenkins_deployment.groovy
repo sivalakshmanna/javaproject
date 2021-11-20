750904bf360c54718da8f3cb50d7cb96c8087ec6
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-20 08:08:58 +0530

    updated script

--- jenkins_deployment.groovy
+++ jenkins_deployment.groovy
@@ -1,10 +1,20 @@
 //declarative pipeline
 pipeline{
     agent any 
+    parameters{
+        string(name: 'BRANCH_NAME', defaultvalue: 'master', descrption: 'from which branch artifacts wants to deploy?')
+         string(name: 'BRANCH_NUM', defaultvalue: '', descrption: 'from which branch artifacts wants to deploy?')
+    }
     stages{
         stage("download artifacts"){
             steps{
                 println "Here I'm downloding artifacts from s3"
+                sh """
+                       aws s3 ls
+                       aws s3 ls s3://sivabandela
+                       aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+                  """
+                       
             }
         }
         stage("copy artifacts"){
