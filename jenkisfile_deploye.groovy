522e3a0babd8ca7d7072795838155c7445683381
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-20 15:37:16 +0530

    updated command

--- jenkisfile_deploye.groovy
+++ jenkisfile_deploye.groovy
@@ -1,25 +1,33 @@
+//declarative pipeline
 pipeline{
-    agent any 
-    parameters {
-        string(name: 'BRANCH_NAME', defaultvalue: 'master', description: 'From which branch artifacts want to deploy?')
-        string(name: 'BUILD_NUM', defaultvalue: '', description: 'From which build number artifacts want to deploy?')
-    }
+    agent any
+   parameters {
+    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'From which branch artifacts want to deploy?')
+    string(name: 'BUILD_NUM', defaultValue: '', description: 'From which build number artifacts want to deploy?')
+    string(name: 'SERVER_IP', defaultValue: '', description: 'To  which want to deploy?')
+
+  }
     stages{
-        stage("download artifacts"){
-            steps{
-                println "Here I'm downloding artifacts from s3"
+        stage("download artifact"){
+            steps {
+                println "Here I'm downloading artifacts from S3"
                 sh """
-                       aws s3 ls
-                       aws s3 ls s3://sivabandela
-                       aws s3 cp s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+                        aws s3 ls
+                        aws s3 ls s3://sivabandela
+                        aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+                        aws s3 cp s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
+ 
                    """
-                       
+                
             }
         }
-        stage("copy artifacts"){
-            steps{
-                println "I'mcopying artifacts from from jenkins to tomcat server"
+        stage("copy artifacts") {
+            steps {
+                println "Here I'm coping artifact from Jenkins to Tomcat servers"
+                sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP} \"systemctl status tomcat\""
+                sh "scp -i /tmp/sivalakshmanna07.pem hello-${BUILD_NUM}.war  ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
             }
         }
     }
 }
+
